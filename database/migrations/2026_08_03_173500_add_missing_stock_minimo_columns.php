382902aeb741c0a9ad9fb5174c05d2d8d7c2f818
FrankOcrospomaUgaz <anthony.2005.rap@gmail.com>
2026-08-03 17:36:18 -0500

    Fix stock threshold migration imports

--- database/migrations/2026_08_03_173500_add_missing_stock_minimo_columns.php
+++ database/migrations/2026_08_03_173500_add_missing_stock_minimo_columns.php
@@ -1,8 +1,8 @@
 <?php
 
-use IlluminateDatabaseMigrationsMigration;
-use IlluminateDatabaseSchemaBlueprint;
-use IlluminateSupportFacadesSchema;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddMissingStockMinimoColumns extends Migration
 {
